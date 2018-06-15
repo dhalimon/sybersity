4f271d33988b2b6a1a118dfd811e7901fc238a04
dhalimon <dhalimon@live.com>
2018-06-15 10:33:14 -0400

    Add files via upload

---
+++ crossplatform2.py
@@ -0,0 +1,14 @@
+import os
+import sys
+input = input("Name to ban?")
+if sys.platform[:3] == 'win':
+    with open("C:\Windows\System32\drivers\etc\hosts", "w") as fd:
+        fd.write("0.0.0.0 " + input + "\n")
+        print ('on Windows')
+else:
+    fd = open("/etc/hosts", "a")
+    #input = input("name to ban?")
+
+    fd.write("0.0.0.0 " + input + "\n")
+    print('on Linux')
+
